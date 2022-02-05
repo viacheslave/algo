47ffb8d14cce7ee7193798b1b036ddcb3c57e8af
Viacheslav Zholnovach <vyajol@gmail.com>
2022-02-05 22:17:52 +0200

    binarysearch: FenwickTree template

---
+++ binarysearch/src/main/java/binarysearch/templates/FenwickTree.java
@@ -0,0 +1,69 @@
+package binarysearch.templates;
+
+public class FenwickTree {
+  private final int[] tree;
+
+  public FenwickTree(int[] arr) {
+    tree = new int[arr.length + 1];
+
+    System.arraycopy(arr,
+      0, tree,
+      1, arr.length);
+
+    for (var i = 1; i < tree.length; i++) {
+      var parent = i + lsb(i);
+
+      if (parent < tree.length)
+        tree[parent] += tree[i];
+    }
+  }
+
+  /**
+   * Returns sum of elements [from, to]
+   * @param from - start el (1-based)
+   * @param to - end el (1-based)
+   * @return range sum
+   */
+  public int sum(int from, int to) {
+    if (from == 1)
+      return sum(to);
+    return sum(to) - sum(from - 1);
+  }
+
+  /**
+   * Add a value to an item at index
+   * @param index - el index
+   * @param value - el extra value
+   */
+  public void add(int index, int value) {
+    var i = index;
+    while (i < tree.length) {
+      tree[i] += value;
+      i = i + lsb(i);
+    }
+  }
+
+  /**
+   * Updates a value at index
+   * @param index - el index
+   * @param value - el extra value
+   */
+  public void set(int index, int value) {
+    var diff = value - tree[index];
+    add(index, diff);
+  }
+
+  private int sum(int to) {
+    var value = 0;
+    while (to > 0) {
+      value += tree[to];
+      var lsb = lsb(to);
+      to -= lsb;
+    }
+    return value;
+  }
+
+  private int lsb(int i) {
+    return Integer.lowestOneBit(i);
+  }
+}
--- binarysearch/src/main/java/binarysearch/templates/Pair.java
+++ binarysearch/src/main/java/binarysearch/templates/Pair.java
@@ -1,2 +1,26 @@
-package binarysearch.templates;public class Pair {
+package binarysearch.templates;
+
+import java.util.Objects;
+
+public class Pair {
+  public int item1;
+  public int item2;
+
+  public Pair(int item1, int item2) {
+    this.item1 = item1;
+    this.item2 = item2;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Pair pair = (Pair) o;
+    return item1 == pair.item1 && item2 == pair.item2;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(item1, item2);
+  }
 }
