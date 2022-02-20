c5b24b58ff19b7a14bc29fd4b2dbeb53f3d421fb
Viacheslav Zholnovach <vyajol@gmail.com>
2022-02-20 14:10:35 +0200

    binarysearch: P0742

--- binarysearch/src/main/java/binarysearch/P0742.java
+++ binarysearch/src/main/java/binarysearch/P0742.java
@@ -6,22 +6,24 @@ import java.util.ArrayList;
  * Problem: https://binarysearch.com/problems/Incrementable-Stack
  * Submission: https://binarysearch.com/problems/Incrementable-Stack/submissions/6656216
  */
-class IncrementableStack {
-  private ArrayList<Integer> arr = new ArrayList<>();
+public class P0742 {
+  class IncrementableStack {
+    private ArrayList<Integer> arr = new ArrayList<>();
 
-  public IncrementableStack() {
-  }
+    public IncrementableStack() {
+    }
 
-  public void append(int val) {
-    arr.add(val);
-  }
+    public void append(int val) {
+      arr.add(val);
+    }
 
-  public int pop() {
-    return arr.remove(arr.size() - 1);
-  }
+    public int pop() {
+      return arr.remove(arr.size() - 1);
+    }
 
-  public void increment(int inc, int count) {
-    for (var i = 0; i < Math.min(count, arr.size()); i++)
-      arr.set(i, arr.get(i) + inc);
+    public void increment(int inc, int count) {
+      for (var i = 0; i < Math.min(count, arr.size()); i++)
+        arr.set(i, arr.get(i) + inc);
+    }
   }
 }
