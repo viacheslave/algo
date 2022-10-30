5c9c1511478150623b003674e36e00d56c5ed60d
Viacheslav Zholnovach <vyajol@gmail.com>
2022-10-30 19:24:31 +0200

    leetcode: P0523

--- leetcode/c#/Problems/P0523.cs
+++ leetcode/c#/Problems/P0523.cs
@@ -37,3 +37,47 @@ internal class P0523
     }
   }
 }
+
+/// <summary>
+///    Problem: https://leetcode.com/problems/continuous-subarray-sum/
+///    Submission: https://leetcode.com/problems/continuous-subarray-sum/submissions/830866738/
+/// </summary>
+internal class P0523_2
+{
+  public class Solution
+  {
+    public bool CheckSubarraySum(int[] nums, int k)
+    {
+      if (nums.Length == 0)
+        return false;
+
+      var prefixSums = new int[nums.Length + 1];
+
+      // calculate prefix sums
+      // and mod them k
+
+      for (int i = 0; i < nums.Length; i++)
+      {
+        prefixSums[i + 1] = prefixSums[i] + nums[i];
+      }
+
+      for (int i = 0; i < prefixSums.Length; i++)
+      {
+        prefixSums[i] %= k;
+      }
+
+      var intervals = prefixSums
+        .Select((p, i) => (p, i))
+        .GroupBy(p => p.p).ToDictionary(p => p.Key, p => p.Select(r => r.i).ToArray());
+
+      // then check the collection of each mod
+      foreach (var interval in intervals)
+      {
+        if (interval.Value.Length > 1 && interval.Value[^1] - interval.Value[0] > 1)
+          return true;
+      }
+
+      return false;
+    }
+  }
+}
